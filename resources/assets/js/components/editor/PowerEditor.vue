2318c144b9756e6c5798779ca8a86ad82f535aec
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2017-08-23 11:08:42 +0800

    Add a state on save changes button on power editor component

--- resources/assets/js/components/editor/PowerEditor.vue
+++ resources/assets/js/components/editor/PowerEditor.vue
@@ -2,14 +2,21 @@
 	<div class="Peditor">
 		<div id="peditor"></div>
 
-        <button type="button" class="btn btn-success" @click="updateSpintaxArticle">Save Spintax Changes</button>
+        <button type="button" class="btn btn-success" @click="updateSpintaxArticle" ref="changesBtn">Save Spintax Changes</button>
         <button type="button" class="btn btn-danger" @click="dissmissSpintaxArticle">Dismiss</button>
+
+        <!-- Misc -->
+        &nbsp;&nbsp;&nbsp;
+		<span v-if="isLoading">LOADING....</span>
 	</div>
 </template>
 
 <script>
 	export default {
 		props: ['article'],
+		data() {
+			return { isLoading: false };
+		},
 		mounted() {
 			this.initSpintax();
 		},
@@ -35,8 +42,14 @@
 					spintax: $('div.note-editable').first().text()
 				};
 
+				this.isLoading = true;
+				this.$refs.changesBtn.disabled = true;
+
 				axios.patch('/words/updateSpintaxArticle', data).then(response => {
 					let data = response.data;
+
+					this.isLoading = false;
+					this.$refs.changesBtn.disabled = false;
 
 					if (data) {
 						this.$emit('isPowerEditorDismiss');  // close the power editor component
