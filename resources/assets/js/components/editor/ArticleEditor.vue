7c78661fac7b5e7166e115c9265480f7ca9ab706
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2017-09-01 10:57:49 +0800

    Change position of timer

--- resources/assets/js/components/editor/ArticleEditor.vue
+++ resources/assets/js/components/editor/ArticleEditor.vue
@@ -320,7 +320,7 @@
         background: #080808;
         color: #fff;
         left: 0;
-        bottom: 38em;
+        bottom: 33em;
         padding-top: 1em;
         padding-left: 1.2em;
         padding-right: .5em;
