088b5daf341141614d0570d068bbf879604af286
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2017-10-05 11:25:40 +0800

    Editor page fix Create Article page style

--- resources/views/editor/create-article.blade.php
+++ resources/views/editor/create-article.blade.php
@@ -1,16 +1,5 @@
 @extends('layouts.editor')
 
-@section ('header')
-    <style>
-        .EditorPage { margin: 3em 0; }
-        .Word { padding: 0 7em; }
-    </style>
-@endsection
-
 @section ('content')
-    <div class="EditorPage">
-        @include('partials._content')
-
-        <create-article user="{{ $user }}" token="{{ csrf_token() }}"></create-article>
-    </div>
+    <create-article user="{{ $user }}" token="{{ csrf_token() }}"></create-article>
 @endsection
