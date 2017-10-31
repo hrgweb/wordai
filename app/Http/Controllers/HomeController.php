041bac9f5573e299ad8f16392e8c3b29f46f7c89
Hergen Roque Gamayon <hergen@brandstrong.co>
2017-10-31 13:46:39 +0800

    Fix when writer login redirect to articles

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
     public function index()
     {
     	// tmp
-    	return redirect('articles');
+    	// return redirect('articles');
 
         return view('home');
     }
