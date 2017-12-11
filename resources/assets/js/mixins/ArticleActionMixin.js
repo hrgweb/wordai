67eb101e776128378d2244d3c848544c1b2a32bf
Hergen Roque Gamayon <hergen@brandstrong.co>
2017-12-11 12:13:49 +0800

    replace the pt being filled to dpt when select domain on create article

--- resources/assets/js/mixins/ArticleActionMixin.js
+++ resources/assets/js/mixins/ArticleActionMixin.js
@@ -5,156 +5,156 @@ import FullArticle from './../components/words/FullArticle.vue';
 import User from './../class/User.js';
 
 export const ArticleActionMixin = {
-	props: [ 'user', 'token' ],
-	components: { Error, SeparateParagraph, FullArticle, CopyscapeApi },
-	data() {
-		return {
-			article: '',
-			wordsMax: 1800,
-			count: 0,
-			paragraph: '',
-			paragraphs: [],
-			spintaxResult: '',
-			isValidationFail: false,
-			errorType: 1, // legend: 1-input validation, 0-fail response result from server
-			spintaxType: 'article',
-			spin: {
-				article_type_id: 'select',
-				doc_title: '',
-				domain_id: 'select',
+    props: ['user', 'token'],
+    components: { Error, SeparateParagraph, FullArticle, CopyscapeApi },
+    data() {
+        return {
+            article: '',
+            wordsMax: 1800,
+            count: 0,
+            paragraph: '',
+            paragraphs: [],
+            spintaxResult: '',
+            isValidationFail: false,
+            errorType: 1, // legend: 1-input validation, 0-fail response result from server
+            spintaxType: 'article',
+            spin: {
+                article_type_id: 'select',
+                doc_title: '',
+                domain_id: 'select',
                 group_id: 0,
                 group_name: '',
-				domain: '',
-				keyword: '',
-				lsi_terms: '',
-				domain_protected: '',
-				article: '',
-				protected: '',
-				synonym: '',
+                domain: '',
+                keyword: '',
+                lsi_terms: '',
+                domain_protected: '',
+                article: '',
+                protected: '',
+                synonym: '',
                 citation: ''
-			},
-			articleTypes: [],
-			isCurated: false,
-			isArticleTypesLoaded: false,
-			domains: [],
-			isDomainNotSet: false,
-			userObj: new User()
-		}
-	},
-	created() {
-		this.authUser = JSON.parse(this.user);
+            },
+            articleTypes: [],
+            isCurated: false,
+            isArticleTypesLoaded: false,
+            domains: [],
+            isDomainNotSet: false,
+            userObj: new User()
+        }
+    },
+    created() {
+        this.authUser = JSON.parse(this.user);
 
-		this.listOfArticleType();
-		this.userDomainList();
-		// this.userDomainSetup();
-	},
-	watch: {
-		articleTypes(data) {
-			this.isArticleTypesLoaded = data.length > 0 ? true : false;
-		},
+        this.listOfArticleType();
+        this.userDomainList();
+        // this.userDomainSetup();
+    },
+    watch: {
+        articleTypes(data) {
+            this.isArticleTypesLoaded = data.length > 0 ? true : false;
+        },
 
-		article(data) {
-			this.spin['spin'] = data;
-			this.postSpinTax(this.spin); // post article
-		}
-	},
-	methods: {
-		wordCount() {
-			let count = 1;
-			let words = this.spin.article.trim();
+        article(data) {
+            this.spin['spin'] = data;
+            this.postSpinTax(this.spin); // post article
+        }
+    },
+    methods: {
+        wordCount() {
+            let count = 1;
+            let words = this.spin.article.trim();
 
-			for (let i=0; i<words.length; i++) {
-				let char = words.charAt(i);
+            for (let i = 0; i < words.length; i++) {
+                let char = words.charAt(i);
 
-				if (char === ' ') {
-					count++;
-				}
-			}
+                if (char === ' ') {
+                    count++;
+                }
+            }
 
-			this.count = count;
-		},
+            this.count = count;
+        },
 
-		spinTax() {
-			this.isLoading = true;
-			this.isValidationFail = false;
-			this.isSuccess = false;
-			this.$refs.spinButton.disabled = true;
+        spinTax() {
+            this.isLoading = true;
+            this.isValidationFail = false;
+            this.isSuccess = false;
+            this.$refs.spinButton.disabled = true;
 
-			axios.post('/words', this.spin).then(response => {
-				let data = response.data;
+            axios.post('/words', this.spin).then(response => {
+                let data = response.data;
 
-				this.isValidationFail = data.isError;
+                this.isValidationFail = data.isError;
 
-				// check if validation fail
-				if (this.isValidationFail === true) {
-					this.errors = data.errors;
-					this.isValidationFail = true;
-					this.isLoading = false;
-				} else {
-					this.isValidationFail = false;
-					this.isLoading = false;
-					this.$refs.spinButton.disabled = false;
+                // check if validation fail
+                if (this.isValidationFail === true) {
+                    this.errors = data.errors;
+                    this.isValidationFail = true;
+                    this.isLoading = false;
+                } else {
+                    this.isValidationFail = false;
+                    this.isLoading = false;
+                    this.$refs.spinButton.disabled = false;
 
-					// check if api response is success
-					if (data.status === 'Success') {
-						let text = data.text;
-						let paragraphs = text.split(/\n\n\n/); // regex expression finding new line
+                    // check if api response is success
+                    if (data.status === 'Success') {
+                        let text = data.text;
+                        let paragraphs = text.split(/\n\n\n/); // regex expression finding new line
 
-						this.spintaxResult = text;
+                        this.spintaxResult = text;
 
-						// article is now the spintax result
-						// display finish full article
-						this.generateFullArticle(this.spin.article);
+                        // article is now the spintax result
+                        // display finish full article
+                        this.generateFullArticle(this.spin.article);
 
-						// post article
-						this.spin['article'] = this.spin.article;
-						this.spin['spintax'] = text;
-					}
+                        // post article
+                        this.spin['article'] = this.spin.article;
+                        this.spin['spintax'] = text;
+                    }
 
-					// check if api response is fail
-					if (data.status === 'Failure') {
-						this.errorType = 0;
-						this.isValidationFail = true;
-						this.errors = data.error;
-					}
-				}
-			});
-		},
+                    // check if api response is fail
+                    if (data.status === 'Failure') {
+                        this.errorType = 0;
+                        this.isValidationFail = true;
+                        this.errors = data.error;
+                    }
+                }
+            });
+        },
 
-		postSpinTax(data) {
-			axios.post('/words/postSpinTax', data).then(response => console.log(response.data));
-		},
+        postSpinTax(data) {
+            axios.post('/words/postSpinTax', data).then(response => console.log(response.data));
+        },
 
-		generateFullArticle(article) {
-			axios.post('/words/generateFullArticle', { article: article }).then(response => {
-				this.article = response.data;
-				this.isSuccess = true;
-			});
-		},
+        generateFullArticle(article) {
+            axios.post('/words/generateFullArticle', { article: article }).then(response => {
+                this.article = response.data;
+                this.isSuccess = true;
+            });
+        },
 
-		generateParagraph(paragraphs) {
-			axios.post('/words/generateParagraph', { paragraphs: paragraphs }).then(response => this.paragraphs = response.data);
-		},
+        generateParagraph(paragraphs) {
+            axios.post('/words/generateParagraph', { paragraphs: paragraphs }).then(response => this.paragraphs = response.data);
+        },
 
-		respinParagraph(payload) {
-			console.log(payload);
-			this.paragraphs[payload.index] = payload.paragraph;
-		},
+        respinParagraph(payload) {
+            console.log(payload);
+            this.paragraphs[payload.index] = payload.paragraph;
+        },
 
-		listOfArticleType() {
-			axios.get('/articleType/listOfArticleType').then(response => this.articleTypes = response.data);
-		},
+        listOfArticleType() {
+            axios.get('/articleType/listOfArticleType').then(response => this.articleTypes = response.data);
+        },
 
-		domainList() {
-			axios.get('/admin/domainList').then(response => this.domains = response.data);
-		},
+        domainList() {
+            axios.get('/admin/domainList').then(response => this.domains = response.data);
+        },
 
-		domainFillIn(isSet, lsi, term, synonym) {
-			this.isDomainNotSet = isSet;
+        domainFillIn(isSet, lsi, term, synonym) {
+            this.isDomainNotSet = isSet;
             this.spin['lsi_terms'] = lsi;
-			this.spin['protected'] = term;
-			this.spin['synonym'] = synonym;
-		},
+            this.spin['domain_protected'] = term;
+            this.spin['synonym'] = synonym;
+        },
 
         setupDomainChange(url) {
             axios.get(url).then(response => {
@@ -162,7 +162,7 @@ export const ArticleActionMixin = {
 
                 if (data) {
                     this.domainFillIn(false, data.lsi_terms, data.protected, data.synonym);
-                    this.groupName('/user/groupName?group_id='+data.group_id);
+                    this.groupName('/user/groupName?group_id=' + data.group_id);
                 } else {
                     this.domainFillIn(true, '', '', '');
                     this.spin['group_id'] = 0;
@@ -180,38 +180,38 @@ export const ArticleActionMixin = {
             });
         },
 
-		domainChange() {
-			let vm = this;
-			// let options = $('datalist#domains').find('option');
-			// let domain_id = this.userObj.getUserId(vm, options, vm.spin.domain);
+        domainChange() {
+            let vm = this;
+            // let options = $('datalist#domains').find('option');
+            // let domain_id = this.userObj.getUserId(vm, options, vm.spin.domain);
             // this.spin.domain_id = domain_id.attributes[1].value;
 
             let domain_id = this.spin.domain_id;
-			let url = '/words/domainChange?domain_id=' + domain_id;
+            let url = '/words/domainChange?domain_id=' + domain_id;
 
-			if (parseInt(domain_id, 10) > 0) {
-				this.setupDomainChange(url);
+            if (parseInt(domain_id, 10) > 0) {
+                this.setupDomainChange(url);
                 this.wordaiBus.getKeywordsAssociatedByDomain(domain_id);
-			} else {
-				this.spin['protected'] = '';
+            } else {
+                this.spin['protected'] = '';
                 this.spin['synonym'] = '';
                 this.spin['group_id'] = 0;
-				this.spin['group_name'] = '';
-			}
-		},
+                this.spin['group_name'] = '';
+            }
+        },
 
-		userDomainSetup() {
-			axios.get('/user/userDomainSetup').then(response => {
-				let data = _.head(response.data);
+        userDomainSetup() {
+            axios.get('/user/userDomainSetup').then(response => {
+                let data = _.head(response.data);
 
-				if (data) {
-					this.spin['domain'] = data.domain;
-					this.spin['domain_id'] = data.domain_id;
-					this.spin['protected'] = data.protected;
-					this.spin['synonym'] = data.synonym;
-				}
-			});
-		},
+                if (data) {
+                    this.spin['domain'] = data.domain;
+                    this.spin['domain_id'] = data.domain_id;
+                    this.spin['protected'] = data.protected;
+                    this.spin['synonym'] = data.synonym;
+                }
+            });
+        },
 
         userDomainList() {
             let user_id = this.authUser.id;
@@ -220,5 +220,5 @@ export const ArticleActionMixin = {
                 this.domains = response.data;
             });
         }
-	}
+    }
 };
